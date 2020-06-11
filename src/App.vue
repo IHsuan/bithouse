c5277bbb927b7b9e92d4ee4ae5c31b36e3db1aa2
ihsuan <frostyjoan0829@livemail.tw>
2020-06-11 15:25:40 +0800

    feature: 搜尋
    fake data ＆ filter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
+      <!-- 搜尋 -->
       <form @submit.prevent class="sticky-top py-3">
         <div class="input-group">
           <div class="input-group-prepend">
@@ -8,28 +9,45 @@
               <i class="fa fa-search" aria-hidden="true"></i>
             </button>
           </div>
-          <input type="text" class="form-control" placeholder="Search" />
+          <input
+            type="text"
+            v-model="form.keyword"
+            class="form-control"
+            placeholder="Search"
+          />
         </div>
       </form>
 
-      <div v-for="i in 3" :key="i" class="mb-5">
+      <!-- 分類＆專輯 -->
+      <div v-for="category in $list" :key="category.id" class="mb-5">
         <h3>
-          title {{ i }}
+          {{ category.title }}
         </h3>
 
         <div class="row">
-          <div v-for="j in 10" :key="j" class="col-4 col-md-3 col-lg-2">
+          <div
+            v-for="album in category.albums"
+            :key="album.id"
+            class="col-4 col-md-3 col-lg-2"
+          >
             <figure class="position-relative">
               <img
-                src="https://fakeimg.pl/1000x1000/aaa/?text=picture"
+                :src="album.picture"
                 class="img-fluid rounded"
-                alt=""
+                :alt="album.name"
               />
 
               <figcaption class="position-absolute text-center">
-                picture {{ i }} - {{ j }}
+                {{ album.name }}
               </figcaption>
             </figure>
+          </div>
+
+          <!-- 查無結果 -->
+          <div class="col-12" v-if="!category.albums.length">
+            <p class="text-center text-secondary">
+              no result...
+            </p>
           </div>
         </div>
       </div>
@@ -38,7 +56,71 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        keyword: "",
+      },
+
+      fakeList: [],
+    };
+  },
+  computed: {
+    $list() {
+      const list = JSON.parse(JSON.stringify(this.fakeList));
+
+      list.forEach((category) => {
+        category.albums = category.albums.filter((album) => {
+          return album.name.indexOf(this.form.keyword) !== -1;
+        });
+      });
+
+      return list;
+    },
+  },
+
+  mounted() {
+    const fakeList = [];
+
+    // 三個分類
+    for (let i = 1; i <= 3; i++) {
+      const category = new Category();
+
+      category.id += i;
+      category.title += i;
+
+      // 每個分類十張專輯
+      for (let j = 1; j <= 10; j++) {
+        const album = new Album();
+
+        const randomNumber = Math.floor(Math.random() * 15) + 1;
+
+        album.id += j;
+        album.name += randomNumber;
+        album.picture += randomNumber;
+
+        category.albums.push(album);
+      }
+
+      fakeList.push(category);
+    }
+
+    this.fakeList = fakeList;
+  },
+};
+
+class Category {
+  id = 0;
+  title = "title";
+  albums = [];
+}
+
+class Album {
+  id = 0;
+  name = "name";
+  picture = "https://fakeimg.pl/1000x1000/aaa/?text=picture";
+}
 </script>
 
 <style lang="scss">
